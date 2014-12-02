fc55d2c553c43b7a9b646b7e7b01fe94605a7fcb
Arif <arif_451@yahoo.com>
2014-12-01 20:59:35 -0500

    java file

---
+++ Java Git Server/src/com/git/test/GitFirstProjet.java
@@ -0,0 +1,15 @@
+package com.git.test;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitFirstProjet 
+{
+
+	public static void main(String[] args) 
+	{
+		FirefoxDriver browser=new FirefoxDriver();
+		browser.get("http:/www.facebook.com");
+
+	}
+
+}
